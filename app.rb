9e2d0636e013ef7be4798a23f0b779c46095cedd
Tammy Metz <tammy@tammymetz.com>
2018-11-11 16:54:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,4 +31,18 @@ class App < Sinatra::Base
     return_string = "#{word1} #{word2} #{word3} #{word4} #{word5}."
   end
 
+  get "/operation/:number1/:number2" do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @operation = params[:operation]
+    if @operation == "add"
+      "#{@num1 + @num2}"
+    elsif @operation == "subtract"
+      "#{@num1 - @num2}"
+    elsif @operation == "multiply"
+      "#{@num1 * @num2}"
+    else
+      "#{@num1 / @num2}"
+  end
+
 end
